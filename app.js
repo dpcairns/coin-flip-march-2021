ea25ba1c11d12f9e90f4ff30ad7555f8da99edc9
harriet-the-spy <dpcairns@gmail.com>
2021-03-25 11:56:04 -0700

    add reset button

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // import functions and grab DOM elements
+import { getActualHeadsOrTails } from './utils.js';
+
 const buttonEl = document.getElementById('guess-button');
 const whoWonEl = document.getElementById('who-won');
 const winsEl = document.getElementById('wins');
 const lossesEl = document.getElementById('losses');
+const resetButton = document.getElementById('reset');
 
 // initialize state
 let wins = 0;
@@ -13,11 +16,32 @@ buttonEl.addEventListener('click', () => {
     // - randomly pick a computer throw ('heads' or 'tails')
     const randomNumber = Math.random();
     const actualHeadsOrTails = getActualHeadsOrTails(randomNumber);
+    
     // - get the user's guess
-    //     - it lives in the radio buttons sooommmmehow???
-    // - compare user guess to computer throw
-    // - display if the user won (show computer throw)
-    // - increment the total
-    // - increment the wins, if the user got it right
-    // - display new values for total, wins, and losses
+    // use querySelector to get the currently selected radio button
+    const currentlySelectedRadioButton = document.querySelector('input:checked');
+
+    const userGuess = currentlySelectedRadioButton.value;
+    
+    total++;
+
+    if (userGuess === actualHeadsOrTails) {
+        wins++;
+        whoWonEl.textContent = 'You won the flip!';
+    } else {
+        whoWonEl.textContent = 'You lost the flip!';
+    }
+
+    winsEl.textContent = wins;
+    lossesEl.textContent = total - wins;
 });
+
+function reset() {
+    whoWonEl.textContent = '';
+    wins = 0;
+    total = 0;
+    winsEl.textContent = 0;
+    lossesEl.textContent = 0;
+}
+
+resetButton.addEventListener('click', reset);
