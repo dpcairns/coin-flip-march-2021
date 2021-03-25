0f5a5b6691b7bc42443084fde1be41ac067db0c6
harriet-the-spy <dpcairns@gmail.com>
2021-03-25 11:13:31 -0700

    add refactors

--- utils.js
+++ utils.js
@@ -1,5 +1,10 @@
 export function getActualHeadsOrTails(someNumber) {
-    if (someNumber >= .5) return 'tails';
+    // if (someNumber >= .5) return 'tails';
 
-    return 'heads';
+    // return 'heads';
+
+    // const something = conditional ? 'if true it is this' : 'if false, it is this';
+    return someNumber >= .5 ? 'tails' : 'heads';
 }
+
+// export const getActualHeadsOrTails = x => x >= .5 ? 'tails' : 'heads';
